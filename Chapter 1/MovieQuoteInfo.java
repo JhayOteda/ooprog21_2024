43e91449e0a747dd4265b5e754a91de63da1ad70
kibaaax <146962089+kibaaax@users.noreply.github.com>
2024-08-16 15:05:58 +0800

    Add files via upload

---
+++ Chapter 1/MovieQuoteInfo.java
@@ -0,0 +1,15 @@
+public class MovieQuoteInfo {
+    public static void main(String[] args) {
+        String movieQuote = "I feel the need...the need for speed.";
+        String movieFrom = "Produced by Don Simpson and Jerry Bruckheimer, with distribution by Paramount Pictures.";
+        String movieCharacter = "Pete Maverick Mitchell, played by Tom Cruise";
+        String movieTitle = "Top Gun";
+        String movieYear = "(1986)";
+        
+        
+        System.out.println("\n" + movieQuote);
+        System.out.println(movieFrom);
+        System.out.println(movieCharacter);
+        System.out.println(movieTitle + " " + movieYear);
+    }
+}
