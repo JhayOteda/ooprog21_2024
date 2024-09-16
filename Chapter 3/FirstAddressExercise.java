d3ddeb76f89c264deeed7cedf9200fcf8277ebbf
Jhay Oteda <146962089+JhayOteda@users.noreply.github.com>
2024-09-16 23:42:38 +0800

    Add files via upload

---
+++ Chapter 3/FirstAddressExercise.java
@@ -0,0 +1,14 @@
+public class FirstAddressExercise {
+
+    public static void main(String[] args) {
+        displayAddress();
+    }
+
+    public static void displayAddress() {
+        
+        System.out.println("Huawei Base.");
+        System.out.println("Bantian, Longgang District");
+        System.out.println("Shenzhen, Guangdong, 518129");
+        System.out.println("China");
+    }
+}
