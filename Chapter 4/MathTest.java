ae60a2d774aeafd7b9076e2f4b143773ab5f7b73
Jhay Oteda <146962089+JhayOteda@users.noreply.github.com>
2024-10-04 23:27:00 +0800

    Add files via upload

---
+++ Chapter 4/MathTest.java
@@ -0,0 +1,33 @@
+public class MathTest
+{
+	public static void main(String[] args) {
+	    double sqrt = Math.sqrt(10);
+		System.out.println("SQRT: " + sqrt);
+		
+		 double sin = Math.sin(15);
+		System.out.println("SIN: " + sin);
+		
+		 double cosine = Math.cos(24);
+		System.out.println("COS: " + cosine);
+		
+		 double floor = Math.floor(20);
+		System.out.println("FLOOR: " + floor);
+		
+		 double ceil = Math.ceil(40);
+		System.out.println("CEIL: " + ceil);
+		
+		 double round = Math.round(7);
+		System.out.println("ROUND: " + round);
+		
+		int maximumNumber = Math.max(25,90);
+		System.out.println("MAX:" + maximumNumber);
+		
+		int minimumNumber = Math.min(77,88);
+		System.out.println("MIN:" + minimumNumber);
+		
+		double randomNumber = Math.random();
+		System.out.println("RANDOM:" + randomNumber);
+		
+		
+	}
+}
